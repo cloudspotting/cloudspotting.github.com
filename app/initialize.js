54925b70528faf5be5cecb3d3ca11d5b2f5445cf
David Gomez-Urquiza <david.gurquiza@gmail.com>
2013-03-03 15:10:09 -0600

    Oh dear zeus. please forgive me :cloud:

--- app/initialize.js
+++
@@ -1,8 +0,0 @@
-var application = require('application')
-var cloudspotting = require('cloudspotting')
-
-$(function() {
-    application.initialize()
-    Backbone.history.start()
-    cloudspotting.initialize()
-})
